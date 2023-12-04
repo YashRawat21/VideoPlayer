b783f3d98fc23c514a33a25243d8132e50ca7c8d
YashRawat21 <rawat17yash@gmail.com>
2023-12-04 23:49:57 +0530

    update commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -158,6 +158,30 @@ const Sidebar = () => {
          } )}
 
        </ul>
+       <ul className='flex flex-col border-b-2 border-gray-700'>
+         {helpLinks.map(({icon ,name}) => {
+           return (
+            <li key={name} className= {`pl-6 py-3 hover:bg-zinc-600 ${name==="Home"? "bg-zinc-600" : ""}`} > 
+              <a href='#' className='flex items-center gap-5'>
+                {icon}
+                <span className='text-sm tracking-wider'>{name}</span>
+              </a>
+            </li>
+           )
+         } )}
+
+       </ul>
+       <ul className='flex gap-2 flex-wrap text-sm p-4 text-zinc-400'>
+        {textLinks[0].map((name) => {
+            return <li key={name}>{name}</li>
+        })}
+       </ul>
+       <ul className='flex gap-2 flex-wrap text-sm p-4 text-zinc-400'>
+        {textLinks[1].map((name) => {
+            return <li key={name}>{name}</li>
+        })}
+       </ul>
+       <span className='px-4 text-sm text-zinc-400'>&copy; 2022 Google</span>
     </div>
   )
 }
